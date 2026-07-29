7a93126be8b6e52bb9854d210e823cb71b9e8882
Arnab Choudhuri <arnabch@kgpian.iitkgp.ac.in>
2026-07-29 14:34:29 +0530

    Bump iOS version to 1.0.2

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ const config: ExpoConfig = {
   name: "Fernly",
   slug: "leaflet",
   scheme: "leaflet",
-  version: "1.0.1",
+  version: "1.0.2",
   orientation: "portrait",
   userInterfaceStyle: "light",
   newArchEnabled: true,
